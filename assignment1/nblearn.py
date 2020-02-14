3ecc12ba4c9c61ff5d728d7601f2995dc340c41a
Zheyu Shen <shenz@Zheyus-MacBook-Pro.local>
2020-02-14 12:27:32 -0800

    complete 1.2 learning the model

--- assignment1/nblearn.py
+++ assignment1/nblearn.py
@@ -1,12 +1,19 @@
 import sys
 import os
+import json
 
 
 def files2dict(files, dict):
+    """
+    Renew the word of bag dictionary from files
+    :param
+        files: list of file path
+        dict: dictionary of word of bag
+    """
     for file in files:
         f = open(file, 'r', encoding='latin1')
         for line in iter(f):
-            tokens = line.strip('\n').split(' ')
+            tokens = line.lower().strip('\n').split(' ')
             for token in tokens:
                 if token not in dict.keys():
                     dict[token] = 1
@@ -16,42 +23,118 @@ def files2dict(files, dict):
 
 
 def read_data(dir):
+    """
+    Load the train data. The directory is like
+    --train
+     |--1
+       |--ham
+         |--0001.txt
+         |--0002.txt
+       |--spam
+         |--0001.txt
+         |--0002.txt
+     |--2
+       |--ham
+         |--0001.txt
+         |--0002.txt
+       |--spam
+         |--0001.txt
+         |--0002.txt
+    :param
+        dir: path to train folder
+    :return:
+        a dictionary contain data read from train folder, including total num of ham
+        docs, total num of spam docs, two bag of word for ham and spam.
+    """
+    num_ham = 0
+    num_spam = 0
     ham = {}
     spam = {}
-    folders = os.listdir(dir)
+    # get the relative path without the hidden files
+    folders = [dir + '/' + i for i in os.listdir(dir) if not i.startswith('.')]
     for folder in folders:
         # read ham files
         ham_dir = folder + '/ham'
-        ham_files = os.listdir(ham_dir)
+        ham_files = [ham_dir + '/' + i for i in os.listdir(ham_dir) if not i.startswith('.')]
+        num_ham += len(ham_files)
         files2dict(ham_files, ham)
         # read spam files
         spam_dir = folder + '/spam'
-        spam_files = os.listdir(spam_dir)
+        spam_files = [spam_dir + '/' + i for i in os.listdir(spam_dir) if not i.startswith('.')]
+        num_spam += len(spam_files)
         files2dict(spam_files, spam)
-    parameters = {'ham': ham,
+    parameters = {'num_ham': num_ham,
+                  'num_spam': num_spam,
+                  'ham': ham,
                   'spam': spam}
     return parameters
 
 
 def learn_model(data):
-    # TODO estimate P(spam) and P(ham) as well as conditional probabilities P(token|spam) and P(token|ham)
-    pass
+    """
+    Estimate P(spam) and P(ham) as well as conditional probabilities P(token|spam) and P(token|ham)
+    :param
+        data: a dict contain spam data and ham data. For example,
+        {'spam': {'token1': 1, 'token2': 3, 'token3': 2},
+         'ham': {'token4': 1, 'token5': 3, 'token6': 2}}
+    :return:
+        a dict contain P(spam) and P(ham) and all conditional probabilities. For example,
+        {'spam': P(spam),
+         'ham': P(ham),
+         'token|spam': P(token|spam),
+         'token|ham': P(token|ham)}
+    """
+    num_spam = data['num_spam']
+    num_ham = data['num_ham']
+    spam = data['spam']
+    ham = data['ham']
+    probability = {}
+
+    # P(spam) = count(spam) / m
+    # P(ham) = count(ham) / m
+    total_file = num_spam + num_ham
+    p_spam = num_spam / total_file
+    p_ham = num_ham / total_file
+    probability['p_spam'] = p_spam
+    probability['p_ham'] = p_ham
+
+    # P(x_i|c) = count(x_i, c) / (\sum_j count(x_j, c))
+    sum_spam = sum(spam.values())
+    sum_ham = sum(ham.values())
+    for token, num in spam.items():
+        probability['p_' + token + '_spam'] = num / sum_spam
+    for token, num in ham.items():
+        probability['p_' + token + '_ham'] = num / sum_ham
+
+    return probability
 
 
 def store_model(probability):
-    # TODO store the probabilities in model file nbmodel.txt
-    pass
+    """
+    Store the probabilities in model file nbmodel.txt with json format
+    :param
+        probability: dictionary of all parameters for NB model.
+        {'h_spam': H(spam),
+         'h_ham': H(ham),
+         'h_token_spam': H(token|spam),
+         'h_token_ham': H(token|ham)}
+    """
+    js_obj = json.dumps(probability)
+    file_object = open('nbmodel.txt', 'w', encoding='latin1')
+    file_object.write(js_obj)
+    file_object.close()
 
 
 def main():
-    if len(sys.argv != 1):
-        print('The argument is a data directory.')
-    input_dir = sys.argv[0]
+    if len(sys.argv) != 2:
+        print('Please enter the right directory.')
+        return
+    input_dir = sys.argv[1]
     # input_dir = './Spam or Ham/train'
     train_data = read_data(input_dir)
     probability = learn_model(train_data)
     store_model(probability)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
